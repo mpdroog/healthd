f090dedc37f6db97c37b7e54b553bc85875792f0
mark <mpdroog@rootdev.nl>
2022-11-29 14:21:29 +0100

    Build. Block on failure to unittest

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
-set -x
-set -u
+set -euo pipefail
+IFS=$'\n\t'
 
+go test ./...
 env GOOS=linux GOARCH=amd64 go build
 echo -n $'\003' | dd bs=1 count=1 seek=7 conv=notrunc of=./healthd
